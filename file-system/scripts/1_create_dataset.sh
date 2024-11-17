a0f600f70f6106bedb0695dba8b1386b430f74bd
Dario B <dario.mail1@gmail.com>
2024-11-17 16:27:14 +0100

    Update 1_create_dataset.sh

--- file-system/scripts/1_create_dataset.sh
+++ file-system/scripts/1_create_dataset.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo dd if=/dev/random of=/home/sdb/key bs=1 count=32
+sudo dd if=/dev/random of=/opt/zfs/encryption_key  bs=1 count=32
 sudo zfs create -o encryption=on -o keyformat=raw -o keylocation=file:///opt/zfs/encryption_key cloud/nextcloud
 
